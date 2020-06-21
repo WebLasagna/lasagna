6d445351e36aa5139b60b9f06ad1d57b95512735
Lasagna <weblasagna@protonmail.com>
2020-06-21 23:54:11 +0200

    Fixed privacy policy

--- src/views/Privacy.vue
+++ src/views/Privacy.vue
@@ -5,12 +5,11 @@
         <h1>{{ $t('privacy.title') }}</h1>
         <span>This policy is effective as of 2 January 2020.</span>
         <p>Your privacy is very important to me. It is my policy to respect it regarding any information I may collect from you across my website, and other sites I own and operate.</p>
-        <p>The informations we collect from you are used for analytical purposes only. We do not sell your informations to third parties.</p>
+        <p>The information I collect associated to you are used for analytical purposes only. I do not sell your informations to third parties.</p>
         <p>I only retain collected information for as long as necessary to provide you with this website. The data stored is protected within commercially acceptable means to prevent loss and theft, as well as unauthorised access, disclosure, copying, use or modification.</p>
         <p>My website links to external sites that I do not operate. Please be aware that I have no control over the content and practices of these sites, and cannot accept responsibility or liability for their respective privacy policies.</p>
-        <p>You are free to refuse our request for your personal information, with the understanding that we may be unable to provide you with some of your desired services.</p>
-        <p>I don’t share any personally identifying information publicly or with third-parties, except when required to by law. We collect data in accordance with the GDPR, so you can express your request to delete data collected during your use of this site by contacting me.</p>
-        <p>Your continued use of our website will be regarded as acceptance of our practices around privacy and personal information. If you have any questions about how we handle user data and personal information, feel free to contact me.</p>
+        <p>I don’t share any personally identifying information publicly or with third-parties, except when required to by law. I collect data in accordance with the GDPR, so you can express your request to delete data collected during your use of this site by contacting me.</p>
+        <p>Your continued use of my website will be regarded as acceptance of my practices around privacy and personal information. If you have any questions about how I handle user data and personal information, feel free to contact me at <a href="mailto:privacy@lasagna.ml">privacy@lasagna.ml</a>.</p>
         <div class="center">
           <router-link to="/">Homepage</router-link>
         </div>
